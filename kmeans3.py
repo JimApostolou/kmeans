bf3a94864f1a4c384b428a3ab40b9182d8610814
NickTheBit <nikosgklu@gmail.com>
2019-11-17 10:37:05 +0200

    Fixed some typos that i couldnt bear to look at... xD

--- kmeans3.py
+++ kmeans3.py
@@ -2,28 +2,30 @@ import random as r
 import math as m
 from matplotlib import pyplot as plt
 import numpy as np
-p=[]
-c=[]
-thesi=[]
-poifile = open("dataset.txt","r")
-points = int(poifile.readline())
-centeroids = 10
-while centeroids > 8:
-    centeroids = int(input("Give me the centeroids you want: "))
-    if centeroids > 8:
-        print('The limit is 8 centeroids')
+
+p = []
+c = []
+thesi = []
+
+profile = open("dataset.txt", "r")
+points = int(profile.readline())
+centroids = 10
+while centroids > 8:
+    centroids = int(input("Give me the centroids you want: "))
+    if centroids > 8:
+        print('The limit is 8 centroids')
 dimensions = 2
 for i in range(points):
     k = []
     for j in range(dimensions):
-        num = float(poifile.readline())
+        num = float(profile.readline())
         k.append(num)
     p.append(k)
-poifile.close()
-for i in range(centeroids):
+profile.close()
+for i in range(centroids):
     k = []
     for j in range(dimensions):
-        num = round(r.uniform(-100,100),2)
+        num = round(r.uniform(-100, 100), 2)
         k.append(num)
     c.append(k)
 for i in range(points):
@@ -32,7 +34,7 @@ for i in range(points):
         sum += (p[i][j]-c[0][j])**2
     min = m.sqrt(sum)
     minthesi = 0
-    for j in range(1,centeroids):
+    for j in range(1, centroids):
         sum = 0.0
         for k in range(dimensions):
             sum += (p[i][k]-c[j][k])**2
@@ -40,17 +42,17 @@ for i in range(points):
             min = m.sqrt(sum)
             minthesi = j
     thesi.append(minthesi)
-flag=0
-while flag==0:
+flag = 0
+while flag == 0:
     count = 0
-    for i in range(centeroids):
+    for i in range(centroids):
         for j in range(dimensions):
             sin = 0
             sum = 0.0
             for k in range(points):
                 if i == thesi[k]:
                     sum += p[k][j]
-                    sin +=1
+                    sin += 1
             if sin != 0:
                 if sum/sin == c[i][j]:
                     count += 1
@@ -63,7 +65,7 @@ while flag==0:
             sum += (p[i][j] - c[0][j]) ** 2
         min = m.sqrt(sum)
         minthesi = 0
-        for j in range(1, centeroids):
+        for j in range(1, centroids):
             sum = 0.0
             for k in range(dimensions):
                 sum += (p[i][k] - c[j][k]) ** 2
@@ -71,13 +73,14 @@ while flag==0:
                 min = m.sqrt(sum)
                 minthesi = j
         thesi[i] = minthesi
-    if count == centeroids*dimensions:
+    if count == centroids*dimensions:
         flag = 1
 for i in range(len(c)):
-    plt.scatter(c[i][0],c[i][1],color='black')
-colors = ['red','yellow','blue','green','purple','orange','brown','pink']
+    plt.scatter(c[i][0], c[i][1], color='black')
+colors = ['red', 'yellow', 'blue', 'green',
+          'purple', 'orange', 'brown', 'pink']
 for i in range(len(thesi)):
-    for j in range(centeroids):
+    for j in range(centroids):
         if thesi[i] == j:
             plt.scatter(p[i][0], p[i][1], color=colors[j])
 plt.show()
